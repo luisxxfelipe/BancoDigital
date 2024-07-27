410a1401814b54f97d55e7a55836687155835622
luisxxfelipe <102532989+luisxxfelipe@users.noreply.github.com>
2024-07-27 00:17:56 -0300

    Ajustando layout das telas

---
+++ app/src/main/java/com/example/bancodigital/presenter/auth/register/RegisterFragment.kt
@@ -0,0 +1,30 @@
+package com.example.bancodigital.presenter.auth.register
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bancodigital.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
